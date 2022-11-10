600fb114e572a73bb38855fa3cce3ff8201af2bd
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2022-11-10 07:14:45 +0600

    added

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}: ${process.env.DB_PASSWORD}@cluster0.hhcmclk.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('Photography service server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Photography service server running on ${port}`);
+})
