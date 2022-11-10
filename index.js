9c8269c08edabc3b219fb2b8ed10d40dfe4f7e71
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2022-11-10 15:36:29 +0600

    review collection added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -10,20 +10,39 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}: ${process.env.DB_PASSWORD}@cluster0.hhcmclk.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hhcmclk.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
-        const serviceCollection = client.db('photographyService').collection('services');
-
+        const serviceCollection = client.db('photographyService').collection('serviceCollection');
+        const reviews = client.db('photographyService').collection('reviews');
         app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/allservices', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviews.insertOne(review);
+            res.send(result);
+        });
+
+
     }
     finally {
 
